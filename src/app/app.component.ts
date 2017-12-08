bb4eea2d26df333895b37b1849e0d8ab6c35c29c
Ferch8 <f-er1016@hotmail.com>
2017-12-08 00:22:32 -0600

    modulo nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+ngOnInit(){
+  firebase.initializeApp({
+    apiKey: "AIzaSyBq7N99JIab23_kgUQ58SIp3HDGv_nVptE",
+    authDomain: "comprasapp-d6265.firebaseapp.com"
+  })
+}
+
+
 }
---
+++ src/app/facturas/facturas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddfraComponent } from './facturas/addfra/addfra.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AddfraComponent]
+})
+export class FacturasModule { }
